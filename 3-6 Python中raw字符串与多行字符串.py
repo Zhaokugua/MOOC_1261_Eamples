7857ee82f67144b57c9dc6eea463f4d6b5a0a3db
Zhaokugua <62331496+Zhaokugua@users.noreply.github.com>
2020-12-01 19:46:35 +0800

    Create 3-6 Python中raw字符串与多行字符串.py

---
+++ 3-6 Python中raw字符串与多行字符串.py
@@ -0,0 +1,8 @@
+#请把下面的字符串用r'''...'''的形式改写，并用print打印出来：
+#
+#'\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
+
+# Enter a code
+s=r'''"To be, or not to be":that is the question.
+Whether it's nobler in the mind to suffer.'''
+print(s)
